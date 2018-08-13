9c10e406217e38407359ec42adb6457cf043414a
masakazutakewaka <takewakamma@gmail.com>
2018-08-14 02:42:19 +0900

    use repository methods in server directly

--- item/server.go
+++ item/server.go
@@ -12,36 +12,30 @@ import (
 )
 
 type itemServer struct {
-	service Service
+	r Repository
 }
 
-func ListenGRPC(s Service, port int) error {
+func ListenGRPC(r Repository, port int) error {
 	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return err
 	}
 	server := grpc.NewServer()
-	pb.RegisterItemServiceServer(server, &itemServer{s})
+	pb.RegisterItemServiceServer(server, &itemServer{r})
 	reflection.Register(server)
 	return server.Serve(listen)
 }
 
 func (s *itemServer) GetItem(ctx context.Context, r *pb.GetItemRequest) (*pb.GetItemResponse, error) {
-	item, err := s.service.GetItem(ctx, r.Id)
+	item, err := s.r.GetItemByID(ctx, r.Id)
 	if err != nil {
 		return nil, err
 	}
-	return &pb.GetItemResponse{
-		Item: &pb.Item{
-			Id:    item.Id,
-			Name:  item.Name,
-			Price: item.Price,
-		},
-	}, nil
+	return &pb.GetItemResponse{Item: item}, nil
 }
 
 func (s *itemServer) GetItems(ctx context.Context, r *pb.GetItemsRequest) (*pb.GetItemsResponse, error) {
-	items, err := s.service.GetItems(ctx, r.skip, r.take)
+	items, err := s.r.ListItems(ctx, r.skip, r.take)
 	if err != nil {
 		return nil, err
 	}
@@ -49,7 +43,7 @@ func (s *itemServer) GetItems(ctx context.Context, r *pb.GetItemsRequest) (*pb.G
 }
 
 func (s *itemServer) PostItem(ctx context.Context, r *pb.PostItemRequest) (*pb.PostItemResponse, error) {
-	item, err := s.service.PostItem(ctx, r.name, r.price)
+	item, err := s.r.InsertItem(ctx, r.name, r.price)
 	if err != nil {
 		return nil, err
 	}
