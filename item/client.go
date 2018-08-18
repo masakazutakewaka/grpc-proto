665da304e70c26c6260df059797c969aa7e0881a
masakazutakewaka <takewakamma@gmail.com>
2018-08-19 03:35:30 +0900

    implement client methods

--- item/client.go
+++ item/client.go
@@ -8,17 +8,51 @@ import (
 )
 
 type Client struct {
-	conn *grpc.Connection
+	conn    *grpc.ClientConn
+	service pb.ItemServiceClient
 }
 
 func NewClient(url string) (*Client, error) {
+	conn, err := grpc.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := pb.NewItemServiceClient(conn)
+	return &Client{conn, client}, nil
 }
 
-func GetItem() {
+func (client *Client) GetItem(ctx Context.context, id int32) (*pb.Item, error) {
+	res, err := client.GetItem(ctx, &pb.GetItemRequest{Id: id})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Item{
+		Id:    res.Item.Id,
+		Name:  res.Item.Name,
+		Price: res.Item.Price,
+	}, nil
 }
 
-func GetItems() {
+func (client *Client) GetItems(ctx Context.context, skip int32, take int32) (*pb.Items, error) {
+	res, err := client.GetItem(ctx, &pb.GetItemRequest{Skip: skip, Take: take})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Items{
+		Items: res.Items,
+	}, nil
 }
 
-func PostItem() {
+func (client *Client) PostItem(ctx Context.context, name string, price int32) (*pb.Item, error) {
+	res, err := client.PostItem(ctx, &pb.PostItemRequest{Name: name, Price: price})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Item{
+		Id:    res.Item.Id,
+		Name:  res.Item.Name,
+		Price: res.Item.Price,
+	}, nil
 }
