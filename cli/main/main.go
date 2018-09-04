41f01faf5d91282fd1aae6966596002144efe3ed
masakazutakewaka <takewakamma@gmail.com>
2018-09-05 01:06:21 +0900

    implement grpc-gateway server

--- cli/main/main.go
+++ cli/main/main.go
@@ -3,34 +3,38 @@ package main
 import (
 	"golang.org/x/net/context"
 	"log"
+	"net/http"
 	"os"
 
-	"github.com/masakazutakewaka/grpc-proto/coordinate"
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+
+	//"github.com/masakazutakewaka/grpc-proto/coordinate"
 	"github.com/masakazutakewaka/grpc-proto/item"
-	"github.com/masakazutakewaka/grpc-proto/user"
+	pb "github.com/masakazutakewaka/grpc-proto/item/pb"
+	//"github.com/masakazutakewaka/grpc-proto/user"
 )
+
+func run(url string) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := pb.RegisterItemServiceHandlerFromEndpoint(ctx, mux, url, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
 
 func main() {
 	itemURL := os.Getenv("ITEM_URL")
-	userURL := os.Getenv("USER_URL")
-	coordinateURL := os.Getenv("COORDINATE_URL")
-
-	userClient, err := user.NewClient(userURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer userClient.Close()
-
-	err = userClient.PostUser(context.Background(), "takewaka")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	user, err := userClient.GetUser(context.Background(), 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(user)
+	//userURL := os.Getenv("USER_URL")
+	//coordinateURL := os.Getenv("COORDINATE_URL")
 
 	itemClient, err := item.NewClient(itemURL)
 	if err != nil {
@@ -54,6 +58,32 @@ func main() {
 	}
 	log.Println(items)
 
+	// grpc gateway
+	defer glog.Flush()
+
+	if err := run(itemURL); err != nil {
+		glog.Fatal(err)
+	}
+}
+
+/*
+	userClient, err := user.NewClient(userURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer userClient.Close()
+
+	err = userClient.PostUser(context.Background(), "takewaka")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := userClient.GetUser(context.Background(), 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(user)
+
 	coordinateClient, err := coordinate.NewClient(coordinateURL)
 	if err != nil {
 		log.Fatal(err)
@@ -70,4 +100,4 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Println(coordinate)
-}
+*/
